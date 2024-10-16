f9f62728d706851b1ae34291293c65778f636adf
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-16 22:56:29 +0400

    Update models.py
    добавил полный код модели из проекта

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
+import uuid
+from django.conf import settings
+from django.db import models
+from django.urls import reverse
+
+from images.services.utils import unique_slugify
+
+
 class Image(models.Model):
     """Модель для хранения изображений"""
     public_id = models.UUIDField(db_index=True, unique=True,
@@ -13,3 +21,25 @@ class Image(models.Model):
     created = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
     total_likes = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Картинка'
+        verbose_name_plural = 'Картинки'
+
+        indexes = [
+            models.Index(fields=['-created']),
+            models.Index(fields=['-total_likes']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = unique_slugify(self, self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
+
